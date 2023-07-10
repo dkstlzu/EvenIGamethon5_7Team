ef327ca10c8a80bb126f664800d1f56e01b5df67
Shin SeockJu <margron@naver.com>
2023-07-10 23:04:54 +0900

    Map data by stage

---
+++ Production/Assets/Scripts/Dev/Editor/ManualSceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MoonBunny.Dev.Editor
+{
+    public class ManualSceneLoader
+    {
+        [MenuItem("Dev/Load Start Scene")]
+        public static void StartScene()
+        {
+            SceneManager.LoadScene("1. Start");
+        }
+    }
+}
--- Production/Assets/Scripts/Dev/Editor/SaveMapData.cs
+++ Production/Assets/Scripts/Dev/Editor/SaveMapData.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MoonBunny.Dev.Editor
 {
@@ -7,7 +9,14 @@ namespace MoonBunny.Dev.Editor
         [MenuItem("Dev/SaveMapData")]
         public static void SaveData()
         {
-            SaveLoadSystem system = new SaveLoadSystem("MapData", "Map", "csv");
+            string fileName = GetFileName();
+            if (fileName == string.Empty)
+            {
+                Debug.LogWarning("WrongScene");
+                return;
+            }
+            
+            SaveLoadSystem system = new SaveLoadSystem("MapData", fileName, "csv");
             
             system.SaveCSV();
         }
@@ -15,9 +24,46 @@ namespace MoonBunny.Dev.Editor
         [MenuItem("Dev/LoadMapData")]
         public static void LoadData()
         {
-            SaveLoadSystem system = new SaveLoadSystem("MapData", "Map", "csv");
+            string fileName = GetFileName();
+            if (fileName == string.Empty)
+            {
+                Debug.LogWarning("WrongScene");
+                return;
+            }
+            
+            SaveLoadSystem system = new SaveLoadSystem("MapData", fileName, "csv");
             
             system.LoadCSV();
+        }
+
+        private static string GetFileName()
+        {
+            string activeSceneName = SceneManager.GetActiveScene().name;
+            string fileName = "";
+
+            switch (activeSceneName)
+            {
+                case SceneName.Stage1:
+                    fileName = "Stage1Map";
+                    break;
+                case SceneName.Stage2:
+                    fileName = "Stage2Map";
+                    break;
+                case SceneName.Stage3:
+                    fileName = "Stage3Map";
+                    break;
+                case SceneName.Stage4:
+                    fileName = "Stage4Map";
+                    break;
+                case SceneName.Stage5:
+                    fileName = "Stage5Map";
+                    break;
+                case SceneName.StageChallenge:
+                    fileName = "StageChallengeMap";
+                    break;
+            }
+
+            return fileName;
         }
     }
 }
