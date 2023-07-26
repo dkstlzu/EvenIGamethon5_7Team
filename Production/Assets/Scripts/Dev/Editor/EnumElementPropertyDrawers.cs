4f9de7358d96dc8f291d66a6755dca21b19dfa4e
Shin SeockJu <margron@naver.com>
2023-07-26 23:05:13 +0900

    EnumDict editor script missin

---
+++ Production/Assets/Scripts/Dev/Editor/EnumElementPropertyDrawers.cs
@@ -0,0 +1,26 @@
+﻿using dkstlzu.Utility;
+using UnityEditor;
+
+namespace MoonBunny.Dev.Editor
+{
+    [CustomPropertyDrawer(typeof(EnumDictElement<FriendName, int>))]
+    public class FrienNameIntElementPropertyDrawer : EnumDictElementPropertyDrawer<FriendName, int> {}
+    
+    [CustomPropertyDrawer(typeof(EnumDictElement<StageName, int>))]
+    public class StageNameIntElementPropertyDrawer : EnumDictElementPropertyDrawer<StageName, int> {}
+    
+    [CustomPropertyDrawer(typeof(ReadOnlyEnumDict<FriendName, int>))]
+    public class ROFriendNameIntDictPropertyDrawer : ReadOnlyEnumDictPropertyDrawer<FriendName, int> {}
+    
+    [CustomPropertyDrawer(typeof(ReadOnlyEnumDict<StageName, int>))]
+    public class ROStageNameIntDictPropertyDrawer : ReadOnlyEnumDictPropertyDrawer<StageName, int> {}
+    
+    [CustomPropertyDrawer(typeof(EnumDict<FriendName, int>))]
+    public class RWFriendNameIntDictPropertyDrawer : EnumDictPropertyDrawer<FriendName, int> {}
+        
+    [CustomPropertyDrawer(typeof(EnumDict<FriendName, float>))]
+    public class RWFriendNameFloatDictPropertyDrawer : EnumDictPropertyDrawer<FriendName, float> {}
+    
+    [CustomPropertyDrawer(typeof(EnumDict<StageName, int>))]
+    public class RWStageNameIntDictPropertyDrawer : EnumDictPropertyDrawer<StageName, int> {}
+}
