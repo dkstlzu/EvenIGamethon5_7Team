0f1de0e8b673b52d18b450003c11f31b44c52436
Shin SeockJu <margron@naver.com>
2023-07-29 00:11:42 +0900

    StoreUI Update

--- Production/Assets/Scripts/UIs/StoreUI.cs
+++ Production/Assets/Scripts/UIs/StoreUI.cs
@@ -4,6 +4,24 @@ namespace MoonBunny.UIs
 {
     public class StoreUI : UI
     {
-        
+        public void OnGatchaClicked()
+        {
+            
+        }
+
+        public void DoGatcha()
+        {
+            
+        }
+
+        public void OnMemoryClicked(string friendName)
+        {
+            
+        }
+
+        public void BuyMemory(FriendName friendName)
+        {
+            
+        }
     }
 }
