796be27d79edb3b7e87d92bd0a0645dc2220f85b
Shin SeockJu <margron@naver.com>
2023-08-01 03:59:34 +0900

    Quest Spec update

--- Production/Assets/Scripts/Data/QuestSpec.cs
+++ Production/Assets/Scripts/Data/QuestSpec.cs
@@ -5,6 +5,14 @@ namespace MoonBunny
     [CreateAssetMenu(fileName = "QuestSpec", menuName = "Specs/QuestSpec", order = 0)]
     public class QuestSpec : ScriptableObject
     {
+        public int Id;
+        public int TargetProgress;
+        public int DependentId = -1;
+        public bool Repeatable;
         public string DescriptionText;
+        public string DescriptionTextOnDisabled = "???????";
+        public string DescriptionTextOnHidden = "???????";
+
+        public QuestReward Reward;
     }
 }
